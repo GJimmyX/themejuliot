5272523d17060fa07be01b4b69b48027164c3d83
GJimmyX <gilbertg.jimmy@gmail.com>
2022-03-03 16:30:50 +0100

    Modifs base thème

--- articles.php
+++ articles.php
@@ -11,21 +11,35 @@ get_header();
     <?php
         $args = array(
             'post_type' => 'articles',
-            'posts_per_page' => 2,
+            'posts_per_page' => 4,
             'paged' => get_query_var('paged'),
         );
         $t = query_posts($args);
 
+    ?>
+
+    <div class="actualites-disposition">
+    <?php
         while ( have_posts() ) : the_post();
     ?>
 
             <div class="actualites-card">
-                <h2><?php the_title(); ?></h2>
+                <?php if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                    } else { ?>
+                        <img src="<?php echo get_template_directory_uri() . '/img/default-image.png'; ?>" alt="<?php the_title(); ?>" />
+                <?php 
+                    } 
+                ?>
+                <a href="<?php the_permalink(); ?>"><h2 class="actualites-title"><?php the_title(); ?></h2></a>
             </div>
 
         <?php endwhile; ?>
-        
-    <?php the_posts_pagination(); ?>
+    </div>
+    
+    <div class="actualites-pagination">
+        <?php the_posts_pagination(); ?>
+    </div>
 </main>
 
 <?php
--- single-articles.php
+++ single-articles.php
@@ -8,20 +8,83 @@ get_header();
 <main class="articles">
     <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-        <div id="articles-image">
-            <?php $image = get_field( 'image' ); ?>
-            <?php if ( $image ) : ?>
-                <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
-            <?php endif; ?>
-        </div>
+        <div class="articles-structure">
+            <div id="articles-image">
+                <?php $image = get_field( 'image' ); ?>
+                <?php if ( $image ) : ?>
+                    <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+                <?php endif; ?>
+            </div>
 
-        <div id="articles-contenu">
-            <h2><?php the_field( 'title' ); ?></h2>
-
-            <?php the_field( 'description' ); ?>
+            <div id="articles-contenu">
+                <div class="articles-descriptif">
+                    <h2><?php the_field( 'titre' ); ?></h2>
+                    <p><?php the_field( 'description' ); ?></p>
+                </div>
+            </div>
         </div>
 
     <?php endwhile; endif; ?>
+
+    <div class="articles-slider">
+        <div class="articles-elements">
+            <?php
+
+                $articlesSlider = new WP_Query();
+                $articlesSlider->query(array(
+                    'post_type' => 'articles',
+                    'posts_per_page' => 2,
+                    )
+                );
+
+                while ($articlesSlider->have_posts() ) : $articlesSlider->the_post();
+            ?>
+
+            <div class="article-structure">
+                <ul class="article-list">
+                    <?php if ( has_post_thumbnail() ) {
+                        the_post_thumbnail();
+                        } else { ?>
+                            <img src="<?php echo get_template_directory_uri() . '/img/default-image.png'; ?>" alt="<?php the_title(); ?>" />
+                    <?php 
+                        } 
+                    ?>
+                    <h2><?php the_title(); ?></h2>
+                </ul>
+            </div>
+
+            <?php endwhile; ?>
+        </div>
+        <div class="articles-elements">
+            <?php
+
+                $articlesSlider = new WP_Query();
+                $articlesSlider->query(array(
+                    'post_type' => 'articles',
+                    'posts_per_page' => 2,
+                    'offset' => 2,
+                    )
+                );
+
+                while ($articlesSlider->have_posts() ) : $articlesSlider->the_post();
+            ?>
+
+            <div class="article-structure">
+                <ul class="article-list">
+                <?php if ( has_post_thumbnail() ) {
+                        the_post_thumbnail();
+                        } else { ?>
+                            <img src="<?php echo get_template_directory_uri() . '/img/default-image.png'; ?>" alt="<?php the_title(); ?>" />
+                    <?php 
+                        } 
+                    ?>
+                    <h2><?php the_title(); ?></h2>
+                </ul>
+            </div>
+
+            <?php endwhile; ?>
+        </div>
+    </div>
 </main>
 
 <?php
