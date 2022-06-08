17c6acd74822a3654014ed43037a54e86fcd656b
GJimmyX <gilbertg.jimmy@gmail.com>
2022-06-08 16:21:32 +0200

    Modifs correctives

--- bureaux_d_etudes.php
+++ bureaux_d_etudes.php
@@ -16,11 +16,11 @@
     <div class="section-adresses-logos-bureaux-d-etudes">
         <div class="adresses-bureaux-d-etudes">
             <div class="bureau-d-etudes-85">
-                <img src="<?php echo get_template_directory_uri() . '/img/Agence85.png' ?>" alt="Carte de localisation de l'Agence">
+                <img src="<?php echo get_template_directory_uri() . '/img/agence85.png' ?>" alt="Carte de localisation de l'Agence">
                 <a href="https://goo.gl/maps/qRQbKatBLNXW2Q8NA" target="_blank">8 rue de l'arée - Vendéopôle La Mongie, 85140 Sainte Florence</a>
             </div>
             <div class="bureau-d-etudes-44">
-                <img src="<?php echo get_template_directory_uri() . '/img/Agence44.png' ?>" alt="Carte de localisation de l'Agence">
+                <img src="<?php echo get_template_directory_uri() . '/img/agence44.png' ?>" alt="Carte de localisation de l'Agence">
                 <a href="https://goo.gl/maps/JUjYFJWjoKQufqsbA" target="_blank">2 Rue Georges Leclanché, 44980 Sainte Luce sur Loire</a>
             </div>
         </div>
